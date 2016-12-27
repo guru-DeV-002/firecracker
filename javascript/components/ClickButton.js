f542e2b9ac5d9594d247f37cb886ffd6f2cd431d
Sahil Lamba <sahil.lamba95@gmail.com>
2016-12-27 22:03:46 +0530

    Add tap delay animation, fix lights

--- javascript/components/ClickButton.js
+++ javascript/components/ClickButton.js
@@ -5,20 +5,24 @@ class ClickButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      active: false
+      active: false,
+      tap: false
     };
     this.toggle = this.toggle.bind(this);
   }
   toggle() {
     this.setState({
-      active: !this.state.active
+      // active: !this.state.active,
+      tap: !this.state.tap
     });
   }
   render() {
     return (
       <div className="clickButton">
         <p>{this.props.title}</p>
-        <div className={'indicator ' + (this.state.active ? 'active' : '')}>
+        <div className={'indicator ' +
+          (this.state.active ? 'active ' : '') +
+          (this.state.tap ? 'tap-delay' : '')}>
           <div className="light"></div>
         </div>
         <button onClick={this.toggle}></button>
