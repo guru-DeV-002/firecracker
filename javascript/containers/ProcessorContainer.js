3932ff8e5c53f42bed89a82168c420a7ef924567
Sahil Lamba <sahil.lamba95@gmail.com>
2016-12-31 18:12:34 +0530

    Add IO ports

--- javascript/containers/ProcessorContainer.js
+++ javascript/containers/ProcessorContainer.js
@@ -60,48 +60,59 @@ class ProcessorContainer extends React.Component {
   }
   render() {
     return (
-      <div className="processor">
-        <div className="upper">
-          <div className="row0">
-            <p>DC IN</p>
-            <p>MIDI In</p>
-            <p>MIDI Out</p>
-            <p style={{marginLeft: '16%'}}>Phones</p>
-            <p>L/Mono Out</p>
-            <p>R Out</p>
-            <p>Input</p>
-          </div>
-          <div className="row1">
-            <div className="edit-save-container">
-              <OutputLevel
-                outputLevel={this.state.outputLevel}
-                handleOutput={this.handleOutput} />
-              <EditSave
-                editing={this.state.editing}
-                toggleEdit={this.toggleEdit}
-                onSave={this.savePatch} />
+      <div className="processor-container">
+        <div className="pins">
+            <div><span></span></div>
+            <div><span></span></div>
+            <div><span></span></div>
+            <div style={{marginLeft: '16%'}}><span></span></div>
+            <div><span></span></div>
+            <div><span></span></div>
+            <div><span></span></div>
+        </div>
+        <div className="processor">
+          <div className="upper">
+            <div className="row0">
+              <p>DC IN</p>
+              <p>MIDI In</p>
+              <p>MIDI Out</p>
+              <p style={{marginLeft: '16%'}}>Phones</p>
+              <p>L/Mono Out</p>
+              <p>R Out</p>
+              <p>Input</p>
             </div>
-            <Display
-              bank={this.state.bankName}
-              patch={this.state.patchName}
-              fx={this.state.activeFx} />
-            <PropertySelector />
+            <div className="row1">
+              <div className="edit-save-container">
+                <OutputLevel
+                  outputLevel={this.state.outputLevel}
+                  handleOutput={this.handleOutput} />
+                <EditSave
+                  editing={this.state.editing}
+                  toggleEdit={this.toggleEdit}
+                  onSave={this.savePatch} />
+              </div>
+              <Display
+                bank={this.state.bankName}
+                patch={this.state.patchName}
+                fx={this.state.activeFx} />
+              <PropertySelector />
+            </div>
+            <div className="row2">
+              <EffectSelector
+                effects={this.state.effects}
+                toggleEffect={this.toggleEffect} />
+            </div>
           </div>
-          <div className="row2">
-            <EffectSelector
-              effects={this.state.effects}
-              toggleEffect={this.toggleEffect} />
+          <div className="row3">
+            <BankSelector
+              decrement={this.decrementBank}
+              increment={this.incrementBank} />
+            <PatchSelector
+              selectPatch={this.selectPatch} />
           </div>
-        </div>
-        <div className="row3">
-          <BankSelector
-            decrement={this.decrementBank}
-            increment={this.incrementBank} />
-          <PatchSelector
-            selectPatch={this.selectPatch} />
-        </div>
-        <div className="pedal">
-          <span className="separator"></span>
+          <div className="pedal">
+            <span className="separator"></span>
+          </div>
         </div>
       </div>
     );
