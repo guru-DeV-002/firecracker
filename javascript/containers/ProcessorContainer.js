6e0db3595873c367f40d5b246b168ec386c63589
Sahil Lamba <sahil.lamba95@gmail.com>
2017-01-02 00:46:03 +0530

    Add states to Processor

--- javascript/containers/ProcessorContainer.js
+++ javascript/containers/ProcessorContainer.js
@@ -23,6 +23,18 @@ class ProcessorContainer extends React.Component {
     this.incrementBank = this.incrementBank.bind(this);
     this.selectPatch = this.selectPatch.bind(this);
     this.handleOutput = this.handleOutput.bind(this);
+    this.decreaseLevel = this.decreaseLevel.bind(this);
+    this.increaseLevel = this.increaseLevel.bind(this);
+
+    this.state.outputLevel = this.state.banks[this.state.activeBank].patches[this.state.activePatch].outputLevel;
+    this.state.bankName = this.state.banks[this.state.activeBank].code + this.get2DigitString(this.state.activePatch + 1);
+    this.state.patchName = this.state.banks[this.state.activeBank].patches[this.state.activePatch].patchName;
+    this.state.activeFx = this.state.banks[this.state.activeBank].patches[this.state.activePatch].effects[0];
+    this.state.effects = this.state.banks[this.state.activeBank].patches[this.state.activePatch].effects;
+  }
+  get2DigitString(num) {
+    num = num.toString();
+    return (num.length == 1 ? ('0' + num) : num);
   }
   toggleEdit() {
     this.setState({
@@ -54,7 +66,8 @@ class ProcessorContainer extends React.Component {
       activeBank: activeBank,
       bankName: newBank,
       patchName: newPatch.patchName,
-      activeFx: newPatch.effects[0]
+      activeFx: newPatch.effects[0],
+      effects: this.state.banks[activeBank].patches[this.state.activePatch].effects
     });
   }
   incrementBank() {
@@ -67,25 +80,52 @@ class ProcessorContainer extends React.Component {
       activeBank: activeBank,
       bankName: newBank,
       patchName: newPatch.patchName,
-      activeFx: newPatch.effects[0]
+      activeFx: newPatch.effects[0],
+      effects: this.state.banks[activeBank].patches[this.state.activePatch].effects
     });
   }
   selectPatch(pid) {
-    let patch = (pid + 1).toString();
-    patch = (patch.length == 1 ? ('0' + patch) : patch);
+    let patch = this.get2DigitString(pid + 1);
     let newBank = this.state.bankName[0];
     let newPatch = this.state.banks[this.state.activeBank].patches[pid];
     this.setState({
       bankName: newBank + patch,
       patchName: newPatch.patchName,
       activePatch: pid,
-      activeFx: newPatch.effects[0]
+      activeFx: newPatch.effects[0],
+      effects: this.state.banks[this.state.activeBank].patches[pid].effects
     });
   }
   handleOutput(newOutput) {
     this.setState({
       outputLevel: newOutput
     });
+  }
+  decreaseLevel(propID) {
+    let newFx = {
+      name: this.state.activeFx.name,
+      active: this.state.activeFx.active,
+      settings: this.state.activeFx.settings
+    };
+    if (typeof newFx.settings[propID] !== 'undefined') {
+      newFx.settings[propID].level = (newFx.settings[propID].level - 1 < 0 ? 0 : newFx.settings[propID].level - 1);
+      this.setState({
+        activeFx: newFx
+      });
+    }
+  }
+  increaseLevel(propID) {
+    let newFx = {
+      name: this.state.activeFx.name,
+      active: this.state.activeFx.active,
+      settings: this.state.activeFx.settings
+    };
+    if (typeof newFx.settings[propID] !== 'undefined') {
+      newFx.settings[propID].level = (newFx.settings[propID].level + 1 > newFx.settings[propID].max ? newFx.settings[propID].max : newFx.settings[propID].level + 1);
+      this.setState({
+        activeFx: newFx
+      });
+    }
   }
   render() {
     return (
@@ -124,7 +164,9 @@ class ProcessorContainer extends React.Component {
                 bank={this.state.bankName}
                 patch={this.state.patchName}
                 fx={this.state.activeFx} />
-              <PropertySelector />
+              <PropertySelector
+                 decreaseLevel={this.decreaseLevel}
+                 increaseLevel={this.increaseLevel} />
             </div>
             <div className="row2">
               <EffectSelector
--- javascript/containers/ProcessorState.js
+++ javascript/containers/ProcessorState.js
@@ -79,66 +79,755 @@ let effects = [
 ];
 
 let ProcessorState = {
-  outputLevel: 50,
   editing: false,
-  activeFx: {
-    name: 'Drive',
-    active: false,
-    settings: [{
-      name: 'Level',
-      level: 0,
-      max: 100
-    }]
-  },
-  bankName: 'A01',
-  patchName: 'Evil Tone',
   activeBank: 0,
   activePatch: 0,
-  effects: effects,
   banks: [
     {
       code: 'A',
       patches: [{
         patchName: 'Evil Tone',
-        effects: effects,
+        effects: [
+          {
+            name: 'Drive',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 10
+            }]
+          }, {
+            name: 'Comp.',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'EQ',
+            active: false,
+            settings: [{
+              name: 'Treble',
+              level: 0,
+              max: 100
+            }, {
+              name: 'High Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Low Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Bass',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Chorus',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Flanger',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Tremolo',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Delay',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Reverb',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }
+        ],
         outputLevel: 50
       }, {
         patchName: 'Driver',
-        effects: effects,
+        effects: [
+          {
+            name: 'Drive',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Comp.',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'EQ',
+            active: false,
+            settings: [{
+              name: 'Treble',
+              level: 0,
+              max: 100
+            }, {
+              name: 'High Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Low Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Bass',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Chorus',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Flanger',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Tremolo',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Delay',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Reverb',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }
+        ],
         outputLevel: 50
       }, {
         patchName: 'Mild Solo',
-        effects: effects,
+        effects: [
+          {
+            name: 'Drive',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Comp.',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'EQ',
+            active: false,
+            settings: [{
+              name: 'Treble',
+              level: 0,
+              max: 100
+            }, {
+              name: 'High Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Low Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Bass',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Chorus',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Flanger',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Tremolo',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Delay',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Reverb',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }
+        ],
         outputLevel: 50
       }]
     }, {
       code: 'B',
       patches: [{
         patchName: 'Blues',
-        effects: effects,
+        effects: [
+          {
+            name: 'Drive',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Comp.',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'EQ',
+            active: false,
+            settings: [{
+              name: 'Treble',
+              level: 0,
+              max: 100
+            }, {
+              name: 'High Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Low Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Bass',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Chorus',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Flanger',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Tremolo',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Delay',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Reverb',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }
+        ],
         outputLevel: 50
       }, {
         patchName: 'Crunch',
-        effects: effects,
+        effects: [
+          {
+            name: 'Drive',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Comp.',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'EQ',
+            active: false,
+            settings: [{
+              name: 'Treble',
+              level: 0,
+              max: 100
+            }, {
+              name: 'High Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Low Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Bass',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Chorus',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Flanger',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Tremolo',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Delay',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Reverb',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }
+        ],
         outputLevel: 50
       }, {
         patchName: 'Bonamassa',
-        effects: effects,
+        effects: [
+          {
+            name: 'Drive',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Comp.',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'EQ',
+            active: false,
+            settings: [{
+              name: 'Treble',
+              level: 0,
+              max: 100
+            }, {
+              name: 'High Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Low Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Bass',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Chorus',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Flanger',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Tremolo',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Delay',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Reverb',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }
+        ],
         outputLevel: 50
       }]
     }, {
       code: 'C',
       patches: [{
         patchName: 'Floyd',
-        effects: effects,
+        effects: [
+          {
+            name: 'Drive',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Comp.',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'EQ',
+            active: false,
+            settings: [{
+              name: 'Treble',
+              level: 0,
+              max: 100
+            }, {
+              name: 'High Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Low Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Bass',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Chorus',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Flanger',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Tremolo',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Delay',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Reverb',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }
+        ],
         outputLevel: 50
       }, {
         patchName: 'Class Rock',
-        effects: effects,
+        effects: [
+          {
+            name: 'Drive',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Comp.',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'EQ',
+            active: false,
+            settings: [{
+              name: 'Treble',
+              level: 0,
+              max: 100
+            }, {
+              name: 'High Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Low Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Bass',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Chorus',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Flanger',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Tremolo',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Delay',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Reverb',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }
+        ],
         outputLevel: 50
       }, {
         patchName: 'Sweet Child',
-        effects: effects,
+        effects: [
+          {
+            name: 'Drive',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Comp.',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'EQ',
+            active: false,
+            settings: [{
+              name: 'Treble',
+              level: 0,
+              max: 100
+            }, {
+              name: 'High Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Low Mids',
+              level: 0,
+              max: 100
+            }, {
+              name: 'Bass',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Chorus',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Flanger',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Tremolo',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Delay',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }, {
+            name: 'Reverb',
+            active: false,
+            settings: [{
+              name: 'Level',
+              level: 0,
+              max: 100
+            }]
+          }
+        ],
         outputLevel: 50
       }]
     },
@@ -146,6 +835,5 @@ let ProcessorState = {
 };
 
 module.exports = {
-  ProcessorState: ProcessorState,
-  effects: effects
+  ProcessorState: ProcessorState
 };
